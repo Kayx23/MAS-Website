6c93c6cf7b1a820828a4594d6b5b2d705a2fa4b6
Traky <dengk7@mcmaster.ca>
2020-11-05 00:17:53 -0500

    updated events

--- js/FullCalendar.js
+++ js/FullCalendar.js
@@ -17,45 +17,46 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     events: [
       {
-        title: 'All Day Event',
-        start: '2020-09-01'
+        title: 'IABA & Willis Towers Watson ',
+        start: '2020-09-02T18:00:00',
+        url: 'https://willistowerswatson.recsolu.com/external/events/DdNUJU0G0WWRc9sdolsdGQ?fbclid=IwAR38ddIcdtQsOs5muljs9X_qVCrbe1hJylFL74hCebhnQgz1M3KW0v9XQ2A'
       },
       {
-        title: 'Long Event',
-        start: '2020-09-07',
-        end: '2020-09-10'
+        title: 'ASNY Virtual Fair',
+        start: '2020-09-13T12:00:00',
+        url: 'https://asny.memberclicks.net/index.php?option=com_jevents&task=icalrepeat.detail&evid=87&Itemid=132&year=2020&month=09&day=13&title=2020-asny-career-fair-virtual&uid=b01723bb7627489fbb2d12882895fac4&fbclid=IwAR3FavLRCX-87taOu11MJ1ydsoX0CqFqFHX6s-GZvG_Mf5ivkRZ7y_4pVXc'
       },
       {
-        groupId: '999',
-        title: 'Repeating Event',
-        start: '2020-09-09T16:00:00'
+        title: 'Manulife Networking',
+        start: '2020-09-09T17:30:00'
       },
       {
-        groupId: '999',
-        title: 'Repeating Event',
-        start: '2020-09-16T16:00:00'
+        title: 'Sun Life Networking',
+        start: '2020-09-22T12:00:00'
       },
       {
-        title: 'Conference',
-        start: '2020-09-11',
-        end: '2020-09-13'
+        title: 'Eckler Info Session',
+        start: '2020-09-17T17:30:00'
       },
       {
-        title: 'Lunch',
-        start: '2020-09-12T12:00:00'
+        title: 'Desjardins Info Session',
+        start: '2020-09-28T15:00:00'
       },
       {
-        title: 'Meeting',
-        start: '2020-09-12T14:30:00'
+        title: 'Canada Life Info Session',
+        start: '2020-09-28T17:00:00'
       },
       {
-        title: 'Birthday Party',
-        start: '2020-09-13T07:00:00'
+        title: 'WTW Meet-the-Team',
+        start: '2020-09-29T12:00:00'
       },
       {
-        title: 'go to CIA website',
-        url: 'https://www.cia-ica.ca/home',
-        start: '2020-09-28'
+        title: 'WTW Meet-the-Team',
+        start: '2020-10-01T12:00:00'
+      },
+      {
+        title: 'Intact Panel',
+        start: '2020-09-29T16:00:00'
       }
     ],
     eventColor: 'rgb(121, 0, 60)'  /* change colour of all events */
